6dfb4a99a743790a0a2e8f56bcc5311fd63d37e2
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-08-07 13:52:03 +0200

    create an array of nodes

--- rust-a-star/src/main.rs
+++ rust-a-star/src/main.rs
@@ -1,11 +1,15 @@
 //! Main script.
 
 extern crate piston_window;
+extern crate graphics;
 
 use piston_window::{
     PistonWindow,
     WindowSettings,
+    Rectangle,
 };
+
+use graphics::rectangle;
 
 fn main() {
 
@@ -21,6 +25,15 @@ fn main() {
     )
     .build()
     .unwrap();
+
+    const NODES_AMOUNT_ON_MAP: usize = 25;
+    let nodes = [
+        Rectangle {
+            color: [0.0, 0.0, 0.3, 1.0], /* blue */
+            shape: rectangle::Shape::Square,
+            border: None,
+        }; NODES_AMOUNT_ON_MAP
+    ];
 
     while let Some(event) = window.next() {
         /* TODO: to define, simply used to maintain the window open */
